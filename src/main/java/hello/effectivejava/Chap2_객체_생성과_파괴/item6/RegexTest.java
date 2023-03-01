f1c4dd923d0037d0edbb29f7c4b79771fdc99806
kimjinwook <jinwook628@gmail.com>
2023-03-01 20:20:25 +0900

    Item 6 : 불필요한 객체 생성을 피하라

---
+++ src/main/java/hello/effectivejava/Chap2_객체_생성과_파괴/item6/RegexTest.java
@@ -0,0 +1,8 @@
+package hello.effectivejava.Chap2_객체_생성과_파괴.item6;
+
+public class RegexTest {
+
+    public static void main(String[] args) {
+        System.out.println(RomanNumerals.isRomanNumeral("MCMLXXVI"));
+    }
+}
---
+++ src/main/java/hello/effectivejava/Chap2_객체_생성과_파괴/item6/RomanNumerals.java
@@ -0,0 +1,13 @@
+package hello.effectivejava.Chap2_객체_생성과_파괴.item6;
+
+import java.util.regex.Pattern;
+
+public class RomanNumerals {
+    private static final Pattern ROMAN = Pattern.compile(
+            "^(?=.)M*(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$"
+    );
+
+    static boolean isRomanNumeral(String s) {
+        return ROMAN.matcher(s).matches();
+    }
+}
